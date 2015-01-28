98a74b8a16267ec17c92e38fec9d78b070e16ed6
dorayo <yomingyo@gmail.com>
2015-01-28 23:46:52 +0800

    Initial Commit

---
+++ HourglassDemo/QYViewController.m
@@ -0,0 +1,29 @@
+//
+//  QYViewController.m
+//  HourglassDemo
+//
+//  Created by jiwei on 15/1/28.
+//  Copyright (c) 2015年 weiji.info. All rights reserved.
+//
+
+#import "QYViewController.h"
+
+@interface QYViewController ()
+
+@end
+
+@implementation QYViewController
+
+- (void)viewDidLoad
+{
+    [super viewDidLoad];
+	// Do any additional setup after loading the view, typically from a nib.
+}
+
+- (void)didReceiveMemoryWarning
+{
+    [super didReceiveMemoryWarning];
+    // Dispose of any resources that can be recreated.
+}
+
+@end
