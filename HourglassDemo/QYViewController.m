f1557531ce39c3cecdcb8fc4747555098c3363fb
susuyan <susuyan@163.com>
2015-05-28 19:54:08 +0800

    实现一个沙漏

--- HourglassDemo/QYViewController.m
+++ HourglassDemo/QYViewController.m
@@ -7,8 +7,14 @@
 //
 
 #import "QYViewController.h"
+#import "JWHourglassView.h"
 
 @interface QYViewController ()
+
+@property (nonatomic, strong) JWHourglassView *hourglass;
+@property (nonatomic, strong) NSTimer *timer;
+@property (nonatomic, assign) NSTimeInterval totalTime;
+@property (nonatomic, strong) NSDate *beginDate;
 
 @end
 
@@ -18,12 +24,32 @@
 {
     [super viewDidLoad];
 	// Do any additional setup after loading the view, typically from a nib.
+    JWHourglassView *hourglass = [JWHourglassView shareHourglassUse:YES];
+    hourglass.center = self.view.center;
+    self.hourglass = hourglass;
+    [self.view addSubview:hourglass];
+    
+    self.beginDate = [NSDate date];
+    self.totalTime = 30.f * 60.f;
+    self.timer = [NSTimer scheduledTimerWithTimeInterval:0.1f target:self selector:@selector(timerFire:) userInfo:nil repeats:YES];
 }
 
 - (void)didReceiveMemoryWarning
 {
     [super didReceiveMemoryWarning];
     // Dispose of any resources that can be recreated.
+}
+
+-(void)timerFire:(NSTimer *)timer{
+    //告诉沙漏剩余时间
+    NSTimeInterval runtime = [[NSDate date] timeIntervalSinceDate:self.beginDate];
+    
+    NSTimeInterval lastTime = self.totalTime - runtime;
+    
+    [self.hourglass refreshViewWithRemaining:lastTime];
+    
+    
+    
 }
 
 @end
